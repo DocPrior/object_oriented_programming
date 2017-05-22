35c432a8ceddc4b99a6b0b33856bdff20b972049
DocPrior <Sara Prior>
2017-05-21 20:39:41 -0400

    add quota restirctions, really done now with base assignment

--- paper_boy.rb
+++ paper_boy.rb
@@ -6,15 +6,25 @@ class Paperboy
     @name = name
     @experience = 0
     @earnings = 0
+    @quota = 0
   end
 
   def quota
-    quota = 50 + (@experience * 0.5)
+    @quota = 50 + (@experience * 0.5)
   end
 
   def deliver(start_address, end_address)
     @earnings += (end_address - start_address) * 0.25
+
     @experience += end_address - start_address
+
+    if end_address - start_address < quota
+      @earnings -= 2.00
+    end
+
+    if end_address - start_address > quota
+      @earnings += (end_address - start_address - quota) * 0.5
+    end
   end
 
   def report
