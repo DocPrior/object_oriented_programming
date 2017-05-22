9abe0758787c99ce95faec1861eff85e3f92505a
DocPrior <Sara Prior>
2017-05-21 20:19:50 -0400

    finish papaerboy, finish base assignment

---
+++ paper_boy.rb
@@ -0,0 +1,41 @@
+class Paperboy
+  attr_accessor :name, :experience
+  attr_reader :earnings
+
+  def initialize(name)
+    @name = name
+    @experience = 0
+    @earnings = 0
+  end
+
+  def quota
+    quota = 50 + (@experience * 0.5)
+  end
+
+  def deliver(start_address, end_address)
+    @earnings += (end_address - start_address) * 0.25
+    @experience += end_address - start_address
+  end
+
+  def report
+    puts "I'm #{@name}, I've deliverd #{@experience} papers and I've earned $#{@earnings} so far!"
+  end
+end
+
+tommy = Paperboy.new("Tommy")
+
+tommy.quota
+
+tommy.deliver(101, 160)
+
+tommy.earnings
+
+tommy.report
+
+tommy.quota
+
+tommy.deliver(50, 102)
+
+tommy.earnings
+
+tommy.report
